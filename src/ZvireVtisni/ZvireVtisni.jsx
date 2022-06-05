3aa5fd9583f8c45982d9f61dd40e8832306a8d0a
Kateřina Baňacká <banackakaterina@gmail.com>
2022-06-05 15:12:04 +0200

    Oprava výšky Home

--- src/ZvireVtisni/ZvireVtisni.jsx
+++ src/ZvireVtisni/ZvireVtisni.jsx
@@ -7,51 +7,70 @@ const ZvireVtisni = () => (
   <div className="container">
     <h1 className="NadpisyH1">ZVÍŘE V TÍSNI</h1>
     <div>
-      <div className='textZvireVTisni'>
+      <div className="textZvireVTisni">
         <p>
-          Český svaz ochránců přírody má <a href="https://play.google.com/store/search?q=zv%C3%AD%C5%99e%20v%20nouzi&c=apps">mobilní aplikaci</a>, která Vám pomůže když ve městě nebo v přírodě najdete zraněné nebo opuštěné zvíře. Aplikace pomocí gps pozice vyhledá záchrannou stanici, v jejíž působnosti se právě nacházíte a nabídne Vám rovnou do stanice zavolat, poslat gps souřadnice místa nálezu, nebo Vás naviguje, pokus se domluvíte na osobním předání zvířete. Můžete zaslat do stanice fotografii zvířete.
+          Český svaz ochránců přírody má{' '}
+          <a href="https://play.google.com/store/search?q=zv%C3%AD%C5%99e%20v%20nouzi&c=apps">
+            mobilní aplikaci
+          </a>
+          , která Vám pomůže když ve městě nebo v přírodě najdete zraněné nebo
+          opuštěné zvíře. Aplikace pomocí gps pozice vyhledá záchrannou stanici,
+          v jejíž působnosti se právě nacházíte a nabídne Vám rovnou do stanice
+          zavolat, poslat gps souřadnice místa nálezu, nebo Vás naviguje, pokus
+          se domluvíte na osobním předání zvířete. Můžete zaslat do stanice
+          fotografii zvířete.
         </p>
-        <p>
-
-        </p>
-        <p>
-          Dispečink záchranných stanic: 774 155 155
-        </p>
-      </div><Alert>
+        <p>Dispečink záchranných stanic: 774 155 155</p>
+      </div>
+      <Alert>
         <h2>DESATERO ZVÍŘE V NOUZI</h2>
-        <p>
-          Najdete-li mládě, pak se přesvědčte zda pomoc skutečně potřebuje, zda je opuštěné.
+        <p className="textDesatero">
+          Najdete-li mládě, pak se přesvědčte zda pomoc skutečně potřebuje, zda
+          je opuštěné.
         </p>
-        <p>
-          Nedotýkejte se a neodebírejte mláďata zajíců, srnčí, jelení, daní a mufloní zvěře, pokud nejsou prokazatelně osiřelá, zraněná nebo uvízlá.
+        <p className="textDesatero">
+          Nedotýkejte se a neodebírejte mláďata zajíců, srnčí, jelení, daní a
+          mufloní zvěře, pokud nejsou prokazatelně osiřelá, zraněná nebo uvízlá.
         </p>
-        <p>
-          Dospělí jedinci patří do rukou odborníků, protože hrozí poranění zachránce či jeho nakažení vzteklinou. Zda nejde o chorobu přenosnou i na člověka pozná pouze odborník (veterinář)!  Dodržujte hygienu!
+        <p className="textDesatero">
+          Dospělí jedinci patří do rukou odborníků, protože hrozí poranění
+          zachránce či jeho nakažení vzteklinou. Zda nejde o chorobu přenosnou i
+          na člověka pozná pouze odborník (veterinář)! Dodržujte hygienu!
         </p>
-        <p>
-          Odborníkům popište místo, vzhled a projevy zraněného zvířete, oni Vám poradí.
+        <p className="textDesatero">
+          Odborníkům popište místo, vzhled a projevy zraněného zvířete, oni Vám
+          poradí.
         </p>
-        <p>
-          Chraňte své zdraví – pokud nevidíte zjevné poranění, může se jednat o onemocnění zvířete.
+        <p className="textDesatero">
+          Chraňte své zdraví – pokud nevidíte zjevné poranění, může se jednat o
+          onemocnění zvířete.
         </p>
-        <p>
-          Ochočená, uměle odchovaná zvířata nepatří do přírody, protože obvykle ztrácejí přirozenou plachost, a mohou být zabita predátorem nebo myslivcem jako podezřelá z nakažení vzteklinou.
+        <p className="textDesatero">
+          Ochočená, uměle odchovaná zvířata nepatří do přírody, protože obvykle
+          ztrácejí přirozenou plachost, a mohou být zabita predátorem nebo
+          myslivcem jako podezřelá z nakažení vzteklinou.
         </p>
-        <p>
-          Ptačí mládě, které potřebuje pomoc, poznáte především podle toho, že ještě není opeřené, sedí na zemi a píská. Ptačího mláděte se můžete dotknout.
+        <p className="textDesatero">
+          Ptačí mládě, které potřebuje pomoc, poznáte především podle toho, že
+          ještě není opeřené, sedí na zemi a píská. Ptačího mláděte se můžete
+          dotknout.
         </p>
-        <p>
-          Ptáčata, která jsou již opeřená, ale ještě neumějí létat, nepotřebují pomoc. Umístěte je na vyvýšené místo (větev, zídka, apod.), jejich rodiče je najdou a nakrmí.
+        <p className="textDesatero">
+          Ptáčata, která jsou již opeřená, ale ještě neumějí létat, nepotřebují
+          pomoc. Umístěte je na vyvýšené místo (větev, zídka, apod.), jejich
+          rodiče je najdou a nakrmí.
         </p>
-        <p>
-          Ježek, který potřebuje pomoc, se obvykle obtížně pohybuje, je malátný, při dotyku rukou nenaježí bodliny, nestočí se do klubíčka.
+        <p className="textDesatero">
+          Ježek, který potřebuje pomoc, se obvykle obtížně pohybuje, je malátný,
+          při dotyku rukou nenaježí bodliny, nestočí se do klubíčka.
         </p>
-        <p>
-          K vodní hladině přimrzne pouze zraněné zvíře. Zkuste ho nejprve vhodně vyplašit, pokud se snaží vzlétnout a nemůže, volejte pomoc.
+        <p className="textDesatero">
+          K vodní hladině přimrzne pouze zraněné zvíře. Zkuste ho nejprve vhodně
+          vyplašit, pokud se snaží vzlétnout a nemůže, volejte pomoc.
         </p>
       </Alert>
     </div>
-  </div >
+  </div>
 );
 
 export default ZvireVtisni;
