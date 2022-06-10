6f53eab0b05ef0b68455bd11a64f82eeeec4d2d3
Kateřina Baňacká <banackakaterina@in-ius.cz>
2022-06-10 12:05:14 +0200

    Mapa a stylování

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -3,52 +3,58 @@ import { Link } from 'react-router-dom';
 import './style.css';
 import { Alert, Button, Row, Col, Container } from 'react-bootstrap';
 
-
 const Home = () => {
   return (
     <>
-      <div>
-        <p className="textKlik">Vítejte na stránkách PetEmergency,</p>
-        <p className="textKlik2">
-          pokud hledáte veterinární pohotovost můžete začít hledat zde:
-        </p>
+      <div
+        class="bt-image"
+        style={{
+          backgroundImage: 'url(/img/mapaCB.png)',
+        }}
+      >
+        <div>
+          <p className="textKlik">Vítejte na stránkách PetEmergency,</p>
+          <p className="textKlik2">
+            pokud hledáte veterinární pohotovost můžete začít hledat zde:
+          </p>
           <Button className="buttonKlik d-flex align-items-center justify-content-center">
-          <Link to="/hledani">
-            <img
-              className="ikonaKlik"
-              src="./img/tlapka_pruhledna.png"
-              alt="obrázek tlapky v kroužku"
-            />
-          </Link>
-        </Button>
+            <Link to="/hledani">
+              <img
+                className="ikonaKlik"
+                src="./img/tlapka_pruhledna.png"
+                alt="obrázek tlapky v kroužku"
+              />
+            </Link>
+          </Button>
+        </div>
+        <Alert className="pouceniAlert">
+          <Container>
+            <Row>
+              <Col sm={8}>
+                <p>
+                  Veškeré informace uvedené na těchto stránkách slouží k
+                  základní orientaci, pokud se Vašemu zvířeti něco děje, volejte
+                  ihned veterináře!
+                  <br />
+                  Než vyjedete na vybranou veterinární pohotovost, doporučujeme
+                  tam nejdříve zavolat a domluvit se na příjezdu a případném
+                  přijetí zvířete k ošetření.
+                </p>
+              </Col>
+              <Col
+                sm={4}
+                className="col d-flex align-items-center justify-content-end"
+              >
+                <a href="mailto:banackakaterina@gmail.com,Sarka7091@gmail.com">
+                  <Button className="mt-1">
+                    Našli jste nesrovnalost? <br /> Napište nám.
+                  </Button>
+                </a>
+              </Col>
+            </Row>
+          </Container>
+        </Alert>
       </div>
-      <Alert className="pouceniAlert">
-        <Container>
-          <Row>
-            <Col sm={8}>
-              <p>
-                Veškeré informace uvedené na těchto stránkách slouží k základní
-                orientaci, pokud se Vašemu zvířeti něco děje, volejte ihned
-                veterináře!
-                <br />
-                Než vyjedete na vybranou veterinární pohotovost, doporučujeme
-                tam nejdříve zavolat a domluvit se na příjezdu a případném
-                přijetí zvířete k ošetření.
-              </p>
-            </Col>
-            <Col
-              sm={4}
-              className="d-flex align-items-center justify-content-end"
-            >
-              <a href="mailto:banackakaterina@gmail.com,Sarka7091@gmail.com">
-                <Button className="mt-1">
-                  Našli jste nesrovnalost? <br /> Napište nám.
-                </Button>
-              </a>
-            </Col>
-          </Row>
-        </Container>
-      </Alert>
     </>
   );
 };
